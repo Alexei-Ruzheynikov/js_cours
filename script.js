8b44a6e0984f8d9129316a3ba6a9119b9e469ed3
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-01 22:57:36 +0300

    1-прямая-трансляция

--- script.js
+++ script.js
@@ -1,153 +1,77 @@
-// let myVar;
-// console.log("myVar ", myVar); //undefined
+// продакшн - это ветка master в гит
 
-// //null - пустое значение
-// console.log(null > 0); //false
-// console.log(null == 0); //false
-// console.log(null >= 0); //true
+// let n = [1, 2, 3];
+// let n = null;
+// присваиваем null - чтобы очистить переменную, но при этом оставляем ее и можем воспользоваться ею потом(временно освобождаем память)
 
-// myVar = 10;
-// console.log(typeof myVar);
-// myVar = "Hello world";
-// console.log(typeof myVar);
-// myVar = true;
-// console.log(typeof myVar);
-// myVar = null;
-// console.log(typeof myVar);
-// myVar = undefined;
-// console.log(typeof myVar);
-// myVar = Symbol();
-// console.log(typeof myVar);
-// myVar = {};
-// console.log(typeof myVar);
+// массив с данными для заказчика в index.html указывать.(для того чтобы он менял данные).
 
-//Всего - 7 типов данных
-// console.log(typeof myVar);
+//покажет все методы console
+// console.log(console);
+//Светофор для удобочитаемости console.log error - красный, warn - желтый
+// console.error(123);
+// console.warn(123);
 
-//  массивы, рег выражения и т д - это тип данных Объект
-// let myArr = [];
-// let regExp = /w+/g;
-// let func = function () {};
-// let error = Error("error message");
-// let date = new Date();
-// console.log(typeof myArr);
+// console.time("1");
+//Здесь наш код. В итоге получим за какое время сработает. Внутри нужен идетификатор, например 1(любая строка), чтобы их связать.
+// console.timeEnd("1");
+//
 
-//Какой тип данных - typeof
+// удобно выволить обьекты в массиве к примеру
+// console.table()
 
-// let myTrue = true;
-// let myFalse = false;
+//Для строк лучше использовать одинарный и обратные одинарные кавычки(для интерпаляции и т д)
+// const str = "Привет";
 
-// console.log(2 === 2);
-// console.log(2 === 3);
+//строки могут принимать эмодзи, юникоды, асси...(ascii)
 
-//Преобразование в Boolean тип (после преобразования все значения будут  иметь true)
-// console.log(Boolean(5));
-// console.log(Boolean("Hello"));
+// const js = "        js      ";
+//trimStart - убирает пробелы слева (trimLeft - синоним)
+// console.log(js.trimStart());
+//trimEnd - убирает пробелы справа (trimRight - синоним)
+// console.log(js.trimEnd());
+//trim - убирает пробелы слева и справа
+// console.log(js.trim());
+//repeat - повторяет,в скобках укажем 1 - 1 раз 2 -2 раза покажет
+// console.log(js.repeat(2));
 
-//Исключение, число 0,undefined,null,NaN,'' принимают значение false
-// console.log(Boolean(0));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(NaN));
-// console.log(Boolean(""));
+// const js = "js";
+//padStart - сделает столько пробелов слева- сколько нужно, указали 10, padEnd - справа.
+// console.log(js.padStart(10));
 
-// || - или && - и ! - отрицание
-// console.log(true || true);
-// console.log(true || false);
-// console.log(false || false);
-// console.log(true && true);
-// console.log(true && false);
-// console.log(false && false);
-// console.log(!true);
-// console.log(!false);
+// const js = "JavaScript Привет мир JS 19.0";
+// includes - есть ли этот элемент - выведет true или false
+// console.log(js.includes("Привет"));
+// const searchStr = "Java";
+//startsWith - строка начинается с этой переменной/строкой; endsWith - c конца
+// console.log(js.startsWith(searchStr));
+// replace - находит подстрорку и изменяет на ту, что указываем
+// console.log(js.replace("Привет", "Hello"));
 
-//Числа бывают целые, дробные(с плавающе точкой), експенсеальная форма, двоичная форма, восьмеричная форма(лучше через букву o), шестнадцатеричная форма, бесконенчость,
-// console.log(50);
-// console.log(8.5);
-// console.log(0.5);
-// console.log(5e6);
-// console.log(0b1111);
-// console.log(0o11);
-// console.log(0x22aacc);
-// console.log(infinity);
-// console.log(NaN);
-// при делении числа на 0 получаем бесконечность
-// 0 разделим на 0, бесконечность разделим на бесконечность, строку умножим на число - получим NaN
+//Усложненное задание
+// 1) Создать переменную num со значением 266219 (тип данных число)
+const num = 266219;
+const strNum = num.toString();
+console.log("strNum", strNum);
 
-//Унарный плюс - для преобразования в тип данных число
-// console.log(+5);
-// console.log(-5);
-// let i = 5;
-//инкремент и декремент
-// console.log(++i);
-// console.log(i++);
-// console.log(i);
-// console.log(i--);
-// console.log(i);
-// console.log(--i);
+// 2) Вывести в консоль произведение (умножение) цифр этого числа
+// Например: число 123, при помощи javaScript получить каждую цифру ( 1, 2, 3 ) и перемножить их.
+// Правильно использовать цикл или методы перебора.
+// let mult = 1;
+// for (let i = 0; i < strNum.length; i++) {
+//   mult *= strNum[i];
+// }
+// console.log(mult);
 
-//Бинарные операторы + - * / %
+//Или этот способ (reduce работает так - acc сохраняет результат и в n подставляется новое число и происходит умножение)
+const arrNum = strNum.split("");
+console.log(arrNum);
+const mult = arrNum.reduce(function (acc, n) {
+  return acc * n;
+});
+// 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+const n = mult ** 3;
+// 4) Вывести в консоль первые 2 цифры полученного числа
+console.log(n.toString().substring(0, 2));
 
-// let n = 15;
-// // n = n + 3;
-// n += 3;
-// // n = n - 6;
-// n -= 6;
-// // n = n / 3;
-// n /= 3;
-// console.log(n);
-
-//Равенства
-// console.log(3 > 2);
-// console.log(3 < 2);
-// console.log(5 >= 3);
-// console.log(10 <= 5);
-// console.log(5 === 5);
-// console.log(5 !== 6);
-// console.log(5 == "5");
-// console.log(5 != "5");
-
-//Метод извлечения корня
-// console.log(Math.sqrt(25));
-
-//Метод pow возводит в степень
-// console.log(Math.pow(5, 3));
-
-//Целое ли число
-// console.log(Number.isInteger(5.5));
-
-// let myString = 'Hell "o" world!';
-// console.log(myString);
-
-// let str = "Hello my Friends!";
-// console.log(str.length);
-//вызывается метод () круглыми скобками вконце
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-//charAt -возвращает символ по индексу
-// console.log(str.charAt(0));
-//charAt уже не используют, можно воспользоваться просто квадратными скобками и индексом
-// console.log(str[0]);
-
-//возвращают подстроку, не меняют исходную строку, а возвращают новую
-// substring - от какого - до конца или от какого до какого
-// console.log(str.substring(6));
-// console.log(str.substring(9, 15));
-// slice - можно указать минус - будет считать с конца
-// console.log(str.slice(6));
-// console.log(str.slice(-8));
-// substr - с какого символа начать и сколько символов взять
-// console.log(str.substr(6, 9));
-// console.log(str);
-
-// indexOf - вернет индекс подстроки в строке, где начинается, если не найдет - вернет -1
-// console.log(str.indexOf("Friends"));
-
-// replace - заменяет одно на другое
-// console.log(str.replace("my Friends", "World"));
-
-// split -разделяет на массив через разделитель
-// let str = "apple, kiwi, orange";
-// console.log(str.split(", "));
-
-// Статьи по уроку
+// От substr - желательно отказаться( т.к. убрали из спецификации)
