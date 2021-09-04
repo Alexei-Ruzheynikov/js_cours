e382f61753669f7a5c45e93b1709b737e6db951d
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-04 11:26:28 +0300

    8-lecture

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 "use strict";
 
-let week = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-let data = new Date();
-
-for (let i = 0; i < week.length; i++) {
-  let html = week[i];
-  if (i === week.length - 2) {
-    console.log(week[5]);
-    continue;
-  }
-  if (i === week.length - 1) {
-    document.write(week[6].italics());
-    continue;
-  }
-  console.log(week[i]);
-}
-
-// не доделал, пока не хватило знаний
+//Теории нет, сразу практика
