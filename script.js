4d8a01c2f19c7a760f7bd726c1deca4669e99d3f
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-05 23:43:42 +0300

    10-task

--- script.js
+++ script.js
@@ -1,88 +1,57 @@
 "use strict";
 
-const collections = document.querySelectorAll(".collection"),
-  elems = document.querySelectorAll(".elem"),
-  secondHead = document.getElementById("second-head");
+const collection = document.querySelector(".books");
+const books = document.querySelectorAll(".book");
+collection.prepend(books[2]);
+collection.prepend(books[5]);
+collection.prepend(books[3]);
+collection.prepend(books[4]);
+collection.prepend(books[0]);
+collection.prepend(books[1]);
 
-console.log(collections);
-console.log(elems);
+const bodyBg = document.querySelector("body");
+bodyBg.setAttribute(
+  "style",
+  "background-image:url(./image/you-dont-know-js.jpg)"
+);
 
-//remove() - удаляет элемент в DOM дереве, но оставляет в переменной
-// elems[3].remove();
-// elems[1].remove();
+const link = books[4].querySelector("h2 a");
+link.textContent = "Книга 3. this и Прототипы Объектов";
 
-//append - берет и перемещает элементы где бы они не были, вставляет элемент в конец родителя
-// collections[1].append(elems[3]);
-// collections[1].append(elems[1]);
+const adv = document.querySelector(".adv");
+adv.remove();
 
-//prepend - аналог append, только вставляет вначало родителя
-// collections[1].prepend(elems[5]);
+const paragraphWrapOne = books[0].querySelector("ul");
+const paragraphOne = books[0].querySelectorAll("li");
+paragraphWrapOne.append(paragraphOne[0]);
+paragraphWrapOne.append(paragraphOne[1]);
+paragraphWrapOne.append(paragraphOne[3]);
+paragraphWrapOne.append(paragraphOne[6]);
+paragraphWrapOne.append(paragraphOne[8]);
+paragraphWrapOne.append(paragraphOne[4]);
+paragraphWrapOne.append(paragraphOne[5]);
+paragraphWrapOne.append(paragraphOne[7]);
+paragraphWrapOne.append(paragraphOne[9]);
+paragraphWrapOne.append(paragraphOne[2]);
+paragraphWrapOne.append(paragraphOne[10]);
 
-// before правую коллекцию вставляет перед левой коллекцией
-// collection[0].before(collections[1]);
+const paragraphWrapFive = books[5].querySelector("ul");
+const paragraphFive = books[5].querySelectorAll("li");
+paragraphWrapFive.append(paragraphFive[0]);
+paragraphWrapFive.append(paragraphFive[1]);
+paragraphWrapFive.append(paragraphFive[9]);
+paragraphWrapFive.append(paragraphFive[3]);
+paragraphWrapFive.append(paragraphFive[4]);
+paragraphWrapFive.append(paragraphFive[2]);
+paragraphWrapFive.append(paragraphFive[6]);
+paragraphWrapFive.append(paragraphFive[7]);
+paragraphWrapFive.append(paragraphFive[5]);
+paragraphWrapFive.append(paragraphFive[8]);
+paragraphWrapFive.append(paragraphFive[10]);
 
-// after - вставляет элемент справа(elems[0]) после элемента слева(elems[4])
-// elems[4].after(elems[0]);
-
-// replaceWith убирает левый элемент и вставляет на его место правый элемент
-// elems[2].replaceWith(elems[3]);
-
-//cloneNode - делает копию элемента - только его тэга, с атрибутом true в скобках - копирует все ноды
-// const elemClone = elems[3].cloneNode(true);
-//Добавляем новый класс нашему клону
-// elemClone.classList.add("newElem");
-// Добавляем новое значения контенту нашего элемента
-// elemClone.textContent = "new elem";
-// collections[1].append(elemClone);
-
-//задаем  и получаем контент элемента
-// elems[4].textContent = "Привет";
-//innerHTML - позволяет использовать тэги
-// elems[2].innerHTML = "<b>Привет</b>";
-// console.log(elems[4].textContent);
-
-//createElement - создаем элемент тэг
-// const newElem = document.createElement("li");
-// newElem.textContent = "Новый элемент";
-// newElem.classList.add("new_elem");
-
-// collections[1].append(newElem);
-// console.log(newElem);
-
-//
-//insertAdjacentText - добавляет текст без затирания в элемент!
-//insertAdjacentText - 1 параметр - место куда хотим вставить(4 варианта), 2 параметр - добавляемый текст; beforebegin - вставляет до элемента, afterend - после элемента, afterbegin - перед текстом элемента, beforeend - после текста элемента
-// secondHead.insertAdjacentText("beforebegin", "beforebegin");
-// secondHead.insertAdjacentText("afterend", "afterend");
-// secondHead.insertAdjacentText("afterbegin", "afterbegin");
-// secondHead.insertAdjacentText("beforeend", "beforeend");
-
-// insertAdjacentElement вставляет как прошлый метод, только элементы, а не текст
-// secondHead.insertAdjacentElement("beforebegin", elems[1]);
-// secondHead.insertAdjacentElement("afterend", elems[2]);
-// secondHead.insertAdjacentElement("afterbegin", elems[3]);
-// secondHead.insertAdjacentElement("beforeend", elems[4]);
-
-// И этот способ добавляет тэги с текстом
-// secondHead.insertAdjacentHTML("beforebegin", "<h3>beforebegin</h3>");
-// secondHead.insertAdjacentHTML("afterend", "<h3>afterend</h3>");
-// secondHead.insertAdjacentHTML("afterbegin", "<h3>afterbegin</h3>");
-// secondHead.insertAdjacentHTML("beforeend", "<h3>beforeend</h3>");
-
-//
-//
-
-//Устаревшие методы, которыми лучше не пользоваться, но знать как работают надо
-const newElem = document.createElement("li");
-newElem.textContent = "Новый элемент";
-newElem.classList.add("new_elem");
-
-// collections[1].appendChild(elems[3]); //append
-
-// collections[0].insertBefore(newElem, elem[4]); // before
-
-// collections[0].insertBefore(elems[5], collections[0].firstChild); // prepend
-
-// collections[0].replaceChild(newElem, elems[4]); //replaceWith
-
-// collections[0].removeChild(elems[2]); //remove
+const paragraphWrapSix = books[2].querySelector("ul");
+const paragraphSix = books[2].querySelectorAll("li");
+paragraphSix[8].insertAdjacentHTML(
+  "afterend",
+  "<li>Глава 8: За пределами ES6</li>"
+);
