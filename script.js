19ab762d85ba6b30a2c1b7a7725b43ea79f0115b
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-06 00:29:25 +0300

    10-dop ver-2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const button = document.querySelector("button");
 const collection = document.querySelector("ul");
-const elems = document.querySelector("li");
 const input = document.querySelector("input");
 button.addEventListener("click", func);
 function func() {
