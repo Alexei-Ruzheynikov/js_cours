9f95a52d67d497f879c2fd84fb8bb3e984f58b06
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-05 19:07:01 +0300

    9-task

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-"use strict";
+// "use strict";
+
+const buttonCalculate = document.getElementById("start");
+const plusTop = document.getElementsByTagName("button")[0];
+const plusBottom = document.getElementsByTagName("button")[1];
+const checked = document.querySelector("#deposit-check");
+const possibleIncome = document.querySelectorAll(".additional_income-item");
+//Там где value - или в одну переменную сделать?
+const budgetDayValue = document.getElementsByClassName("budget_day-value");
+const expensesMonthValue = document.getElementsByClassName(
+  "expenses_month-value"
+);
+const additionalIncomeValue = document.getElementsByClassName(
+  "additional_income-value"
+);
+const additionalExpensesValue = document.getElementsByClassName(
+  "additional_expenses-value"
+);
+const incomePeriodValue = document.getElementsByClassName(
+  "income_period-value"
+);
+const targetMonthValue = document.getElementsByClassName("target_month-value");
+const budgetMonthValue = document.querySelector(".budget_month-value");
+const targetAmount = document.querySelector(".target-amount");
+const additionalExpensesItem = document.querySelector(
+  ".additional_expenses-item"
+);
+const expensesTitle = document.querySelector(".expenses-title");
+const expensesAmount = document.querySelector(".expenses-amount");
+const incomeTitle = document.querySelector(".income-title");
+const incomeAmount = document.querySelector(".income-amount");
+const salaryAmount = document.querySelector(".salary-amount");
+const periodSelect = document.querySelector(".period-select");
 
 let money;
 let start = function () {
