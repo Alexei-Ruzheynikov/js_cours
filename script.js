097cccbb1947fe78573786327aa97aaeb8e92499
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-06 16:12:33 +0300

    2 ver

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ button.addEventListener("click", func);
 function func() {
   collection.insertAdjacentHTML("beforeend", "<li>" + input.value + "</li>");
 }
+
+// function getListContent() {
+//   let result = [];
+
+//   for (let i = 1; i <= 3; i++) {
+//     let li = document.createElement("li");
+//     li.append(i);
+//     result.push(li);
+//   }
+
+//   return result;
+// }
+
+// ul.append(...getListContent());
