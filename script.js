5942641314cc20482525282f9c0161f0495181f3
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-07 15:58:09 +0300

    12-lecture

--- script.js
+++ script.js
@@ -1,115 +1,78 @@
 "use strict";
 
-// let square = document.querySelector(".square");
-// console.dir(square);
-// let count = 0;
+const inputText = document.getElementById("myText"),
+  myBtn = document.getElementById("myBtn"),
+  text = document.getElementById("text");
 
-// square.onclick = function () {
-//   if (count === 3) {
-//     console.log("Опачки");
-//     square.onclick = null;
-//     return;
-//   }
-//   count++;
-//   console.log("Вы кликнули на квадрат");
+// const showText = function () {
+//   text.textContent = localStorage.myText;
 // };
 
-// square.onclick = function () {
-//   console.log("Это 2 функция");
+// myBtn.addEventListener("click", function () {
+//   localStorage.myText = inputText.value;
+//   showText();
+// });
+// showText();
+
+//
+//
+
+// const showText = function () {
+//   text.textContent = sessionStorage.myText;
 // };
 
-// let clicked = function () {
-//   count++;
-//   if (count === 3) square.removeEventListener("click", clicked);
-//   console.log("Произошел клик по квадрату");
+// myBtn.addEventListener("click", function () {
+//   sessionStorage.myText = inputText.value;
+//   showText();
+// });
+// showText();
+
+// sessionStorage - сохраняет только на время сессии (только пока открыта вкладка)
+//localStorage - хранится вечно, пока не будет удалена вручную или через javascript
+
+//
+//
+
+// const showText = function () {
+//   text.textContent = localStorage.getItem("memory");
 // };
 
-// square.addEventListener("click", clicked);
-
-// square.addEventListener("click", function () {
-//   console.log("Произошел клик по квадрату 2 ");
-// });
-// square.addEventListener("click", function () {
-//   console.log("Произошел клик по квадрату 3");
+// myBtn.addEventListener("click", function () {
+//   localStorage.setItem("memory", inputText.value);
+//   showText();
 // });
 
-//
-//
+// localStorage.removeItem("myText");
 
-// let square = document.querySelector(".square");
+// showText();
 
-// let eventFunc = function (event) {
-//   console.log(event.type);
-// };
-
-// square.addEventListener("click", eventFunc);
-
-//mouseup - при нажатии мышки, когда отпускаешь ее
-// square.addEventListener("mouseup", eventFunc);
-//mousedown -нажимаем кнопку мыши но не отпускаем на объекте
-// square.addEventListener("mousedown", eventFunc);
-//mousemove - событие наведение на объект
-// square.addEventListener("mousemove", eventFunc);
-
-//mouseenter - когда мышку заводим на наш объект
-// square.addEventListener("mouseenter", eventFunc);
-//mouseleave - когда мышку уводим с объекта
-// square.addEventListener("mouseleave", eventFunc);
-//mouseover -  сраатывае при наведении на внутренний элемент какого-либо объекта
-// square.addEventListener("mouseover", eventFunc);
-//mouseout - сраатывае при уходе с элемента и его дочерних элементов какого-либо объекта
-// square.addEventListener("mouseout", eventFunc);
+// Через setItem создаем , через getItem - получаем, removeItem - удаление какой-то localStorage
 
 //
 //
-//DOMContentLoaded когда страница загружена запускает скрипты, обычно оборачивают весь код js в эту конструкцию
-// document.addEventListener("DOMContentLoaded", function () {
-//   console.log("Страница загрузилась");
-// });
-
-// let eventFunc = function (event) {
-//   console.log(event.type);
-//   console.log(event.target.value);
-// };
-//input - когда меняем value у input
-// document.querySelector("#text").addEventListener("input", eventFunc);
-//change - срабатывает, когда после ввода в input убираем фокус(кликаем в другом мместе кроме inputа)
-// keyup - отпускаем кнопку
-// document.querySelector("#text").addEventListener("keyup", eventFunc);
-//keydown - нажимаем кнопку
-// document.querySelector("#text").addEventListener("keydown", eventFunc);
-//focus - когда кликаем на элементи  у нас появляется возможность вводить текст
-// document.querySelector("#text").addEventListener("focus", eventFunc);
-//blur - когда теряем фокус
-// document.querySelector("#text").addEventListener("blur", eventFunc);
-//change - при изменении значения отрабатывает change для type range
-// document.querySelector("#range").addEventListener("change", eventFunc);
-
-// document.querySelector("#link").addEventListener("click", function (event) {
-//   //preventDefault - отменяет стандартное поведение
-//   event.preventDefault();
-//   console.log("click");
-// });
-
 //
-//
-//contextmenu - отменяет меню при клике правой кнопки мыши - можем создать свое!
-// document.addEventListener("contextmenu", function (event) {
-//   //preventDefault - отменяет стандартное поведение
-//   event.preventDefault();
-//   console.log("click");
-// });
 
-let clickElem = null;
-function greenHundler(event) {
-  if (clickElem) {
-    clickElem.classList.remove("green");
+// document.cookie = "имя=значение";
+// document.cookie = "имя2=значение2";
+// document.cookie = "имя3=значение3";
+// document.cookie = "имя=значение4";
+// document.cookie = "hope=life; expires=Tue, 7 May 2024 00:00:00 GMT";
+
+function setCookie(key, value, year, month, day, path, domain, secure) {
+  let cookieStr = encodeURI(key) + "=" + encodeURI(value);
+  if (year) {
+    const expires = new Date(year, month - 1, day);
+    cookieStr += "; expires=" + expires.toGMTString();
   }
-  clickElem = event.currentTarget;
-  clickElem.classList.add("green");
+  cookieStr += path ? "; path=" + encodeURI(path) : "";
+  cookieStr += domain ? "; domain=" + encodeURI(domain) : "";
+  cookieStr += secure ? "; secure=" : "";
+
+  document.cookie = cookieStr;
 }
-//всплытие, а захват событие - пишем еще true
-document
-  .querySelector(".event_btn")
-  .addEventListener("click", greenHundler, true);
-document.querySelector("body").addEventListener("click", greenHundler, true);
+
+setCookie("Привет", "Мир");
+setCookie("Любимый праздник детей", "Новый год", 2022, 1, 1);
+
+console.log(decodeURI(document.cookie));
+//По умолчанию cookie сохраняются на 1 сессию - это значит, что при перезаходе их не будет
