068a84f0bd497e0088ff698adebc45d554cd3d18
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-09-17 18:21:57 +0300

    практика 25 лекции

--- script.js
+++ script.js
@@ -23,32 +23,49 @@
 //
 
 //Про валидацию
-const myForm = document.getElementById("myform");
+// const myForm = document.getElementById("myform");
 
-myForm.addEventListener("submit", valid);
+// myForm.addEventListener("submit", valid);
 
-const elementsForm = [];
+// const elementsForm = [];
 
-for (const elem of myForm.elements) {
-  if (elem.tagName.toLowerCase() !== "button" && elem.type !== "button") {
-    elementsForm.push(elem);
+// for (const elem of myForm.elements) {
+//   if (elem.tagName.toLowerCase() !== "button" && elem.type !== "button") {
+//     elementsForm.push(elem);
+//   }
+// }
+// console.log(elementsForm);
+
+// function valid(event) {
+//   const patternPhone = /^\d+$/;
+//   elementsForm.forEach((elem) => {
+//     if (!elem.value) {
+//       elem.style.border = "1px solid red";
+//       event.preventDefault();
+//     } else {
+//       elem.style.border = "";
+//     }
+
+//     if (elem.id === "phone" && !patternPhone.test(elem.value)) {
+//       elem.style.border = "1px solid red";
+//       event.preventDefault();
+//     }
+//   });
+// }
+
+// Практика
+//
+//
+const valid = new Validator({
+  selector: "#myform",
+  pattern: {
+    phone: /^\+380\d{7}$/,
+    zip: /\d{5,6}/
+  },
+  method: {
+    phone: [["notEmpty"], ["pattern", "phone"]],
+    email: [["notEmpty"], ["pattern", "email"]]
   }
-}
-console.log(elementsForm);
+});
 
-function valid(event) {
-  const patternPhone = /^\d+$/;
-  elementsForm.forEach((elem) => {
-    if (!elem.value) {
-      elem.style.border = "1px solid red";
-      event.preventDefault();
-    } else {
-      elem.style.border = "";
-    }
-
-    if (elem.id === "phone" && !patternPhone.test(elem.value)) {
-      elem.style.border = "1px solid red";
-      event.preventDefault();
-    }
-  });
-}
+valid.init();
